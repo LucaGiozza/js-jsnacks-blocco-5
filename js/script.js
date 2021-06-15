ee53a417e408f3be6f49a5f6575d4d0244ec0b4e
Luca Giozza <luca.giozza@gmail.com>
2021-06-15 14:48:52 +0200

    esercizio 2

--- js/script.js
+++ js/script.js
@@ -50,3 +50,105 @@
      }
  }
  console.log(pesoMin);
+
+
+            //  ESERCIZI0 2
+//  Crea un array di 10 oggetti che rappresentano una zucchina,
+ //  indicandone per ognuno varietà, peso e lunghezza.
+ // Calcola quanto pesano tutte le zucchine.
+
+//  creo un array di 10 oggetti
+ var zucchine = [
+    {
+     'varietà' : 1,
+      'peso' : 2,
+      'lunghezza' : 50
+
+
+    },
+    {
+     'varietà' : 2,
+     'peso' : 1,
+     'lunghezza' : 60
+   
+   
+    },
+    {
+     'varietà' : 3,
+     'peso' : 3,
+     'lunghezza' : 30
+   
+   
+    },
+    {
+     'varietà' : 4,
+     'peso' : 2,
+     'lunghezza' : 25
+   
+   
+    },
+    {
+     'varietà' : 5,
+     'peso' : 1,
+     'lunghezza' : 70
+   
+   
+    },
+    {
+     'varietà' : 6,
+     'peso' : 4,
+     'lunghezza' : 67
+   
+   
+    },
+    {
+     'varietà' : 7,
+     'peso' : 5,
+     'lunghezza' : 55
+   
+   
+    },
+    {
+     'varietà' : 8,
+     'peso' : 2,
+     'lunghezza' : 45
+   
+   
+    },
+    {
+     'varietà' : 9,
+     'peso' : 1,
+     'lunghezza' : 67
+   
+   
+    },
+    {
+     'varietà' : 10,
+     'peso' : 3,
+     'lunghezza' : 34
+   
+   
+    }
+
+
+ ];
+//  creo una variabile che ha come valore di partenza 0
+
+ var pesoTot = 0;
+
+
+//  calcolo la somma del peso di tutte le zucchine
+
+
+ for(var i = 0; i < zucchine.length; i++){
+   
+       
+       pesoTot = pesoTot + zucchine[i].peso;
+
+}
+console.log(pesoTot);
+
+
+
+
+
